a73839d1ca9ce1c22ee0b61bb33d1b460f1fd032
kimkyunghoon <byul2sol@naver.com>
2019-11-22 21:11:31 +0900

    FootholdGenerator 추가 1. Foothold Prefab 추가 및 Prefabs 폴더 생성 2. FootholdGenerator Gameobject 생성하고 스크립트 장착

---
+++ appjam-unity/Assets/Scripts/FootholdGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootholdGenerator : MonoBehaviour
+{
+    public GameObject footholdPrefab;
+    float span = 1.0f;
+    float delta = 0;
+    float py = 0;
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            
+            this.delta = 0;
+            GameObject foothold = Instantiate(footholdPrefab) as GameObject;
+            // x축 최소 수치 ~ 최댓값에서 생성
+            float px = Random.Range(-1.91f, 1.88f);
+            foothold.transform.position = new Vector2(px, py);
+            py += 2.0f;
+
+            // todo
+            // py가 캐릭터의 이동에 따라 생성되게
+        }
+                
+    }
+}
