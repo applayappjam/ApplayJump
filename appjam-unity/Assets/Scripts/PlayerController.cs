f55ed79ba6d190eab23482dfce9e855ec9412177
youseokhwan <tjrghks115@naver.com>
2019-11-22 20:59:06 +0900

    플레이어 임시 구현

--- appjam-unity/Assets/Scripts/PlayerController.cs
+++ appjam-unity/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
-
+    float movePower = 0.1f;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(new Vector2(-movePower, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(new Vector2(movePower, 0));
+        }
+
+
+    }
+    void OnCollisionEnter2D(Collision2D col){
+
+        if(col.gameObject.tag == "Foothold"){
+            rig.AddForce(transform.up*10.0f,ForceMode2D.Impulse);
+        }
     }
 }
